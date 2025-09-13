2543cae8063cf9f882850d15bd2258100194c496
FoudreTeam <poudremod@gmail.com>
2025-09-13 16:39:14 +0200

    1.0.0

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://54.37.70.174:25583/smplauncher.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
